7e1f2524f72aa4affcff3b2b55005ee6a5c4765c
Everett Maness <emaness09@gmail.com>
2016-02-09 22:31:31 -0800

    Added render_template() functionality in method return statements.

--- finalProject/finalProject.py
+++ finalProject/finalProject.py
@@ -2,52 +2,58 @@ from flask import Flask, render_template, request, redirect, url_for, flash, jso
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+#Fake Restaurants
+restaurant = {'name': 'The CRUDdy Crab', 'restaurant_id': '1'}
+restaurants = [{'name': 'The CRUDdy Crab', 'restaurant_id': '1'}, {'name':'Blue Burgers', 'restaurant_id':'2'},{'name':'Taco Hut', 'restaurant_id':'3'}]
+
+#Fake Menu Items
+items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
+item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
+
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/restaurants')
 def viewRestaurants():
 
-	return "This is the page to view all the restaurants"
+	return render_template('restaurants.html', restaurants=restaurants)
 
 @app.route('/restaurant/<int:restaurant_id>/')
 @app.route('/restaurant/<int:restaurant_id>/menu/')
 def restaurantMenu(restaurant_id):
 	#restaurant_id
-	return "This is the page to view restaurant menu items"
-
+	return render_template('menu.html', restaurant=restaurant, items=items)
 
 @app.route('/restaurant/new/')
 def newRestaurant():
 
-	return "This is the page to create a new restaurant"
-
+	return render_template('newrestaurant.html')
 
 @app.route('/restaurant/<int:restaurant_id>/edit/')
 def editRestaurant(restaurant_id):
 	#restaurant_id
-	return "This is the page to edit a restaurant's name"
+	return render_template('editrestaurant.html', restaurant=restaurant)
 
 
 @app.route('/restaurant/<int:restaurant_id>/delete/')
 def deleteRestaurant(restaurant_id):
 	#restaurant_id
-	return "This is the page to delete a restaurant"
+	return render_template('deleterestaurant.html', restaurant=restaurant)
 
 @app.route('/restaurant/<int:restaurant_id>/menu/new/')
 def newMenuItem(restaurant_id):
 	#restaurant_id
-	return "This is the page to add a menu item"
+	return render_template('newmenuitem.html', restaurant=restaurant)
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit/')
 def editMenuItem(restaurant_id, menu_id):
 	#restaurant_id, menu_id
-	return "This is the page to edit a menu item"
+	return render_template('editmenuitem.html', restaurant_id=restaurant_id, item=item)
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete/')
 def deleteMenuItem(restaurant_id, menu_id):
 	#restaurant_id, menu_id
-	return "This is the page to delete a menu Item"
+	return render_template('deletemenuitem.html', restaurant=restaurant, items=items)
 
 
 
